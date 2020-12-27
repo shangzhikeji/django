7088fd61d8ee3f7d9d81818e5dd197b92d6ca241
unknown <shangzhikeji@gmail.com>
2020-12-27 22:05:40 +0800

    重复记录

--- django/contrib/admin/options.py
+++ django/contrib/admin/options.py
@@ -514,7 +514,7 @@ class ModelAdmin(BaseModelAdmin):
     list_display_links = ()
     list_filter = ()
     list_select_related = False
-    list_per_page = 100
+    list_per_page = 20
     list_max_show_all = 200
     list_editable = ()
     search_fields = ()
